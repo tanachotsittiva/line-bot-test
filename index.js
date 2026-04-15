2c74eddaad4e83af37dce1bfb3c98d9bbb60092d
tanachotsittiva <tanachot.sittiva@gmail.com>
2026-04-15 16:00:57 +0700

    first commit my line bot

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const line = require('@line/bot-sdk');
+
+const app = express();
+
+const config = {
+  channelAccessToken: 'ใส่_ACCESS_TOKEN_ตรงนี้',
+  channelSecret: 'ใส่_SECRET_ตรงนี้'
+};
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then(() => res.end())
+    .catch(err => console.error(err));
+});
+
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  const client = new line.Client(config);
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: 'คุณพิมพ์ว่า: ' + event.message.text
+  });
+}
+
+app.listen(3000, () => {
+  console.log('Server running...');
+});
